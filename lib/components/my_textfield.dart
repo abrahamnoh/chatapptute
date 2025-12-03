c6ba51d22c39b9911f912e5dcdf77a0d0021a0ef
Abraham Noh <abrahamnoh01@gmail.com>
2025-12-03 17:44:15 -0600

    Scroll_chat

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -5,12 +5,14 @@ class MyTextField extends StatelessWidget {
   final String hintText;
   final bool obscureText;
   final TextEditingController controller;
+  final FocusNode? focusNode;
   
   const MyTextField({
     super.key,
     required this.hintText,
     required this.obscureText,
     required this.controller,
+    this.focusNode,
     });
 
   @override
@@ -20,6 +22,7 @@ class MyTextField extends StatelessWidget {
       child: TextField(
         obscureText: obscureText,
         controller: controller,
+        focusNode: focusNode,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), //se utiliza el tertiary para que el borde cambie de color segun el tema
